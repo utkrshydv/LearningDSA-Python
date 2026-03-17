8400d64e9d0ae880dbaef21cae09d9e05bfaf6f7
utkrshydv <utkarsh4socials@gmail.com>
2026-03-18 01:32:13 +0530

    bst-deleteNode

--- 03_BinarySearchTree.py
+++ 03_BinarySearchTree.py
@@ -47,6 +47,37 @@ class BinarySearchTreeNode:
 
         return elements
 
+    def find_max(self):
+        if self.right is None:
+            return self.data
+        return self.right.find_max()
+
+    def find_min(self):
+        if self.left is None:
+            return self.data
+        return self.left.find_min()
+    
+    def delete(self, val):
+        if val < self.data:
+            if self.left:
+                self.left = self.left.delete(val)
+        elif val > self.data:
+            if self.right:
+                self.right = self.right.delete(val)
+        else:
+            if self.left is None and self.right is None:
+                return None
+            elif self.left is None:
+                return self.right
+            elif self.right is None:
+                return self.left
+            
+            min_val = self.right.find_min()
+            self.data = min_val
+            self.right = self.right.delete(min_val)
+
+        return self
+
 
 def build_tree(elements):
     print("Building tree with these elements:", elements)
